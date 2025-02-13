d77863499008552384401defee4bd8044ccaf6c1
Dinu98 <alexdinescu888@gmail.com>
2025-02-13 18:19:32 +0200

    Adding ingress-nginx and test route

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -6,6 +6,10 @@ app.use(json());
 
 const port = 3000;
 
+app.get('/api/users/currentUser', (req, res) => {
+  res.send('Hello');
+});
+
 app.listen(port, () => {
   console.log(`Listening on port ${port}`);
 });
